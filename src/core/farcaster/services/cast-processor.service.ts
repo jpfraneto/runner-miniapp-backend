c6245f687f47554d637590554409a624cef782f9
Jorge Pablo Franetovic Stocker <jpfraneto@gmail.com>
2025-10-20 10:11:55 -0300

    aoja

--- src/core/farcaster/services/cast-processor.service.ts
+++ src/core/farcaster/services/cast-processor.service.ts
@@ -185,7 +185,7 @@ export class CastProcessorService {
       const notification = {
         title: `@${username} just ran`,
         body: `${distance} on ${duration}!`,
-        target_url: `https://runnercoin.lat`,
+        target_url: `https://runnercoin.lat/cast/${castData.hash}`,
       };
 
       // Send notification to all users (empty array means all users with notifications enabled)
